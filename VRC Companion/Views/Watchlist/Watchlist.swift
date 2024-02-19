a87f978b3c2bb361b26909e76af07ceaa0f25ac7
DensityInfinite <douglas_jiang@hotmail.com>
2024-02-19 15:04:29 +1100

    Built watchlist tab structure

---
+++ VRC Companion/Views/Watchlist/Watchlist.swift
@@ -0,0 +1,27 @@
+//
+//  Watchlist.swift
+//  VRC Companion
+//
+//  Created by Douglas Jiang on 19/2/2024.
+//
+
+import SwiftUI
+
+struct Watchlist: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Section("Teams") {
+                    Text("Team Identifier")
+                }
+            }
+            .navigationTitle("Watchlist")
+        } detail: {
+            Text("Select one to view details.")
+        }
+    }
+}
+
+#Preview {
+    Watchlist()
+}

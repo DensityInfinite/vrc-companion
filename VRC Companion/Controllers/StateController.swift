8f5c05f43486884caf455adc039aab66990ed869
DensityInfinite <douglas_jiang@hotmail.com>
2024-06-23 16:23:34 +1000

    Added a global state controller

---
+++ VRC Companion/Controllers/StateController.swift
@@ -0,0 +1,12 @@
+//
+//  StateController.swift
+//  VRC Companion
+//
+//  Created by Douglas Jiang on 12/6/2024.
+//
+
+import Foundation
+
+final class StateController: ObservableObject {
+    @Published var userTeamNumber = "1051X"
+}

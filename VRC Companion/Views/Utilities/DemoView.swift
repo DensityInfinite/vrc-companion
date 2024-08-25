f30b3edf961ae3a9b595f59fc9df7e86c93d61b4
DensityInfinite <douglas_jiang@hotmail.com>
2024-08-25 20:41:57 +1000

    Added DemoView placeholder

---
+++ VRC Companion/Views/Utilities/DemoView.swift
@@ -0,0 +1,34 @@
+//
+//  DemoView.swift
+//  VRC Companion
+//
+//  Created by Douglas Jiang on 25/8/2024.
+//
+
+import SwiftUI
+
+struct DemoView: View {
+    var title: String
+    var titleStyle: NavigationBarItem.TitleDisplayMode
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image(systemName: "truck.box.badge.clock")
+                    .font(.title)
+                    .imageScale(.large)
+                Text("This screen is not included in this demo.")
+                    .font(.headline)
+                Text("Development continues after task submission.")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+            }
+            .navigationTitle(title)
+            .navigationBarTitleDisplayMode(titleStyle)
+        }
+    }
+}
+
+#Preview {
+    DemoView(title: "Uh oh", titleStyle: .automatic)
+}

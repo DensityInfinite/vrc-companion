772619ecc2bd6ee899e97df9cdbbf1f75501b8c5
DensityInfinite <douglas_jiang@hotmail.com>
2024-08-27 23:05:49 +1000

    Added dev button for marking and demonstrating welcome screen

--- VRC Companion/Views/Lookup/LookupView.swift
+++ VRC Companion/Views/Lookup/LookupView.swift
@@ -12,9 +12,40 @@ struct LookupView: View {
     
     var body: some View {
         NavigationStack {
-            DemoView(title: "Lookup", titleStyle: .automatic)
+            VStack {
+                Spacer()
+                DemoView(title: "Lookup", titleStyle: .automatic)
+                Spacer()
+                // Button to reset the saved version and show the welcome screen on next launch or when the user opens ContentView() again. (For marking and demonstration purposes only)
+                Button(action: {
+                    removeSavedVersion()
+                }, label: {
+                    ZStack {
+                        Text("Show Welcome on Next Launch")
+                            .font(.headline)
+                            .foregroundStyle(.background)
+                            .frame(height: 45)
+                            .containerRelativeFrame(.horizontal, { length, axis in
+                                if axis == .vertical {
+                                    return length / 3.0
+                                } else {
+                                    return length / 1.3
+                                }
+                            })
+                            .background(RoundedRectangle(cornerRadius: 12))
+                    }
+                })
+                .padding(.bottom, 35)
+            }
         }
         .searchable(text: $searchText)
+    }
+}
+
+extension LookupView {
+    // FOR DEMONSTRATION PURPOSES ONLY. A function to clear the saved key and allows presentation of the welcome screen again.
+    func removeSavedVersion() {
+        UserDefaults.standard.removeObject(forKey: "savedVersion")
     }
 }
 

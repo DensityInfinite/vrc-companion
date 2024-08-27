ed945e588f5cc2d229f0a7a0a35b2da9e1c0c3c0
DensityInfinite <douglas_jiang@hotmail.com>
2024-08-27 23:04:04 +1000

    Added accent colour

---
+++ VRC Companion/Views/Utilities/SplashScreenRow.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenRow.swift
+//  VRC Companion
+//
+//  Created by Douglas Jiang on 27/8/2024.
+//
+
+import SwiftUI
+
+struct SplashScreenRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenRow()
+}

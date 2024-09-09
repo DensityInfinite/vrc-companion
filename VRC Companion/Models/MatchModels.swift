b8340d55cb6b8a668d3d5cb09354bfd8713cb91b
densityinfinite <douglas_jiang@hotmail.com>
2024-09-09 12:10:16 +1000

    Fixed matchlist model mismatch

--- VRC Companion/Models/MatchModels.swift
+++ VRC Companion/Models/MatchModels.swift
@@ -19,7 +19,6 @@ struct MatchModel: Identifiable {
     let scheduledTime: Date?
     let startedTime: Date?
     let field: String?
-    let session: Int
     let scored: Bool
     
     let alliances: [AllianceModel]
@@ -72,7 +71,7 @@ extension MatchModel: Decodable {
         case matchNum = "matchnum"
         case scheduledTime = "scheduled"
         case startedTime = "started"
-        case id, name, event, division, round, instance, field, session, scored, alliances
+        case id, name, event, division, round, instance, field, scored, alliances
     }
     
     init(from decoder: Decoder) throws {
@@ -89,7 +88,6 @@ extension MatchModel: Decodable {
         scheduledTime = try container.decodeIfPresent(Date.self, forKey: .scheduledTime)
         startedTime = try container.decodeIfPresent(Date.self, forKey: .startedTime)
         field = try container.decodeIfPresent(String.self, forKey: .field)
-        session = try container.decode(Int.self, forKey: .session)
         scored = try container.decode(Bool.self, forKey: .scored)
         
         alliances = try container.decode([AllianceModel].self, forKey: .alliances)
